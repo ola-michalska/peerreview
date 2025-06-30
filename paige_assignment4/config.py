2d7261773711ce0715ad21282f18775d89ff2942
Paige Sharma <paigelaurensharma@gmail.com>
2025-06-30 10:08:31 +0100

    Adding paige's assignment 4 folder to be peer reviewed - thanks guys :)!

---
+++ paige_assignment4/config.py
@@ -0,0 +1,7 @@
+#SQL credentials to connect to db
+#please update with your SQL details - esp password
+
+HOST = "localhost"
+USER = "root"
+PASSWORD = "<enter your password here>"
+DATABASE = "my_music"
